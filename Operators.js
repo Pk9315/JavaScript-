d4aa1b6160b8ade7472fc078fc7e097de6d38532
Pk9315 <95037361+Pk9315@users.noreply.github.com>
2023-12-24 16:22:43 +0530

    Add files via upload

---
+++ Operators.js
@@ -0,0 +1,104 @@
+//Arithmetic operator
+// let a=8;
+// let b=6
+// console.log("a+b = ", a+b);
+
+// console.log("a-b = ", a-b);
+
+// console.log("a*b = ", a*b);
+
+// console.log("a/b = ", a/b);
+
+// console.log("a%b = ", a%b); //this is modulus operator.
+
+// console.log("a**b = ", a**b); // this is exponentiation.
+
+/*there are two unique operator which is unary operator.
+1. increment operator(a++,post(++a, pre)).
+2. decrement operator(a--,post,(--a,pre)).*/
+// let a=8;
+// let b=6;
+// console.log("a = ",a, "& b =", b); 
+// a= a++;
+// b = b++
+// console.log("a = ",a);
+// console.log("b =", b);
+// console.log("a++ =", a++);
+// console.log("a = ", a);
+// console.log("++a = ", ++a);
+// console.log("--b = ", --b);
+// console.log("b-- =", b--);
+// console.log("b = ", b);
+
+// Assignment Operators
+
+// let a=5;
+// let b=4;
+// a += 4;
+// console.log("a = ", a); // this is plus equal to
+
+// this is minus equal to 
+// let a=5;
+// let b=6
+// a -= 2
+// console.log(" a =", a);
+
+// this is multiply equal to.
+// let a=5
+// let b=6
+// a *= a
+// console.log("a = ", a);
+
+// this is divide equal to.
+// let a= 6
+// let b=4
+// b /= b
+// console.log(" b =", b);
+
+//this is modulus equal to.
+// let a=8
+// let b=8
+// b %= b
+// console.log("b = ", b)
+
+//this is exponentiation equal to
+// let a=4
+// let b=4
+// a **= a
+// console.log("a = ", a)
+
+
+//Comparison Operators.
+// let a=4
+// let b=8
+// //let b="5"
+// console.log("a == b = ", a==b); //equal to
+// console.log("a != b =", a != b); //not equal to
+// console.log("a === b =", a===b); //equal to and type(strict. it check the data types)
+// console.log("a !== b =", a!==b); //not equal to and type
+// console.log("a > b =", a>b); //it is greater than
+// console.log("a < b =", a<b); //it is less than
+// console.log("a >= b =", a>=b); //it is greater than and equal to
+// console.log("a <= b =", a<=b); //it is less than equal to
+
+//Logical Operators
+//AND Operator(&&)
+
+// let a=6
+// let b=5
+// let cond1= a>b;
+// let cond2= a==6;
+// let cond3= a==5;
+// console.log("cond1 && cond2 =", cond1 && cond2);
+// console.log("cond1 && cond3 =", cond1 && cond3);
+
+//logical OR Operator(||)
+// let a=5
+// let b=6
+// console.log("cond1 || cond2 =", a<b || a==b);
+
+//Logical NOT Operator(!)
+// let a=6
+// let b=6
+// console.log("cond1 ! =", !(a==b));
+// console.log("cond2 ! =", !(a>b));
