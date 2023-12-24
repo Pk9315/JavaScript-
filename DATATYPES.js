d56016cde493d345c6bda18e5a16c00c1940b36c
Pk9315 <95037361+Pk9315@users.noreply.github.com>
2023-12-24 12:35:17 +0530

    Add files via upload

---
+++ DATATYPES.js
@@ -0,0 +1,32 @@
+let age = 24;
+let price= 100
+let fullname = 'pritam'
+isFollow= true;
+let x;
+let y=null;
+
+const student={
+    name:'Rahul name',
+    age: 24,
+    cgpa: 8.2,
+    isPass: true
+};
+student['age']=student['age']+1;
+console.log(student['age'])
+student['name']='Rahul sharma'
+console.log(student['name']);
+
+const product={
+    name: 'Ball pen',
+    rating:4,
+    offer:5,
+    price:200,
+};
+const profile={
+    name: 'Rahul',
+    post: 1-500,
+    profession: 'engineer',
+    followers: 500,
+    following: 200,
+    isFollow: false,
+};
---
+++ first.js
@@ -0,0 +1,14 @@
+let name= "tony stark"
+let age=24;
+let price=1000;
+console.log(name);
+console.log(age);
+
+{
+    let a=5;
+    console.log(a);
+}
+{
+    let b=10
+    console.log(b)
+}
